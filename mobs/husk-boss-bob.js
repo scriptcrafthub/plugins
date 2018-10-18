4273f4b0b149052cb3a3509ca6a0ed092290b05b
Tim Marwick <triyuga@gmail.com>
2018-10-18 22:05:36 +1000

    Create husk-boss-bob.js - compiled from TS

---
+++ mobs/husk-boss-bob.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var entities = require('entities');
+var items = require('items');
+var Vector = Java.type('org.bukkit.util.Vector');
+var log_1 = require("magikcraft/log");
+var log = log_1.Logger('mobs/husk-boss');
+function huskBoss(loc) {
+    var mob = loc.world.spawnEntity(loc, entities['husk']());
+    mob.getEquipment().setItemInHand(items['diamondSword'](1));
+    // mob.getEquipment().setBoots(items['diamondBoots'](1));
+    // mob.getEquipment().setLeggings(items['diamondLeggings'](1));
+    // mob.getEquipment().setChestplate(items['diamondChestplate'](1));
+    mob.getEquipment().setHelmet(items['diamondHelmet'](1));
+    // mob.setCustomName('Bob the Terrible');
+    // behavior loop
+    function loop() {
+        // Keep looping while mob is alive.
+        if (mob.isDead()) {
+            return;
+        }
+        else {
+            setTimeout(function () {
+                loop();
+            }, 5000);
+        }
+        // Fireball Attack! (when haz target).
+        var target = mob.getTarget();
+        if (target && target.type == 'PLAYER') {
+            var fb = Java.type("org.bukkit.entity.Fireball").class;
+            var pl = target.location;
+            var ml = mob.location;
+            var vec = new Vector(pl.x - ml.x, pl.y - ml.y, pl.z - ml.z).normalize();
+            mob.launchProjectile(fb, vec);
+        }
+    }
+    loop();
+    return mob;
+}
+exports.huskBoss = huskBoss;
